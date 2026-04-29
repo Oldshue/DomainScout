6a787327f3791ce66e721583c0e2e0493b8a56b3
oldshue <hamp@superprompt.com>
2026-04-28 19:16:00 -0500

    Fix Namecheap stream: marketplace not godaddy-auction

--- scrapers/namecheap.js
+++ scrapers/namecheap.js
@@ -91,7 +91,7 @@ async function scrapeTLD(tld, maxPages) {
 
         results.push({
           ...parsed,
-          stream:        'godaddy-auction', // auction stream
+          stream:        'marketplace',
           source:        'Namecheap',
           auction_price: item.price ?? null,
           auction_end:   item.endDate ?? null,
